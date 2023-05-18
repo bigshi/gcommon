c29c07df36c7f75f656a52fbd215b0688ebf8e79
luc <lcmusic1994@gmail.com>
2023-05-18 16:47:03 +0800

    init

--- pkg/gutil/time_utils.go
+++ pkg/gutil/time_utils.go
@@ -1,7 +1,6 @@
 package gutil
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -15,69 +14,53 @@ func GetNowUnixMills() int64 {
 }
 
 //
-// ToDateTimeStr
+// GetTodayMinTimestamp
+//  @Description: 今日零点毫秒
+//  @return int64
+//
+func GetTodayMinTimestamp() int64 {
+	timeStr := time.Now().Format("2006-01-02")
+	t, _ := time.Parse("2006-01-02", timeStr)
+	return t.UnixNano() / 1000000
+}
+
+//
+// FormatDateTime
 //  @Description: 格式化时间
 //  @param t
 //  @return string
 //
-func ToDateTimeStr(t time.Time) string {
+func FormatDateTime(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
 }
 
 //
-// ToDateStr
+// FormatDate
 //  @Description: 格式化日期
 //  @param t
 //  @return string
 //
-func ToDateStr(t time.Time) string {
+func FormatDate(t time.Time) string {
 	return t.Format("2006-01-02")
 }
 
 //
-// ToDateStrOfSample
+// FormatDate2
 //  @Description: 格式化日期-简易
 //  @param t
 //  @return string
 //
-func ToDateStrOfSample(t time.Time) string {
+func FormatDate2(t time.Time) string {
 	return t.Format("20060102")
 }
 
 //
-// FromStrToDateOfSample
-//  @Description: 获取日期-简易
+// ParseDate
+//  @Description: 解析成日期
 //  @param timeStr
 //  @return time.Time
 //
-func FromStrToDateOfSample(timeStr string) time.Time {
-	loc, _ := time.LoadLocation("Local")
-	theTime, _ := time.ParseInLocation("20060102", timeStr, loc)
-	return theTime
-}
-
-//
-// FromStrToDateTime
-//  @Description: 获取时间
-//  @param timeStr
-//  @return time.Time
-//
-func FromStrToDateTime(timeStr string) time.Time {
-	if len(timeStr) > 19 {
-		timeStr = timeStr[0:19]
-	}
-	loc, _ := time.LoadLocation("Local")
-	dateTime, _ := time.ParseInLocation("2006-01-02 15:04:05", timeStr, loc)
-	return dateTime
-}
-
-//
-// FromStrToDate
-//  @Description: 获取日期
-//  @param timeStr
-//  @return time.Time
-//
-func FromStrToDate(timeStr string) time.Time {
+func ParseDate(timeStr string) time.Time {
 	if len(timeStr) > 10 {
 		timeStr = timeStr[0:10]
 	}
@@ -87,12 +70,42 @@ func FromStrToDate(timeStr string) time.Time {
 }
 
 //
-// FromTimestampToDateTime
+// ParseDate2
+//  @Description: 解析成日期-简易
+//  @param timeStr
+//  @return time.Time
+//
+func ParseDate2(timeStr string) time.Time {
+	if len(timeStr) > 8 {
+		timeStr = timeStr[0:8]
+	}
+	loc, _ := time.LoadLocation("Local")
+	theTime, _ := time.ParseInLocation("20060102", timeStr, loc)
+	return theTime
+}
+
+//
+// ParseDateTime
+//  @Description: 解析成时间
+//  @param timeStr
+//  @return time.Time
+//
+func ParseDateTime(timeStr string) time.Time {
+	if len(timeStr) > 19 {
+		timeStr = timeStr[0:19]
+	}
+	loc, _ := time.LoadLocation("Local")
+	dateTime, _ := time.ParseInLocation("2006-01-02 15:04:05", timeStr, loc)
+	return dateTime
+}
+
+//
+// ToDateTime
 //  @Description: 转时间戳
 //  @param timestamp
 //  @return time.Time
 //
-func FromTimestampToDateTime(timestamp int64) time.Time {
+func ToDateTime(timestamp int64) time.Time {
 	return time.Unix(timestamp/1000, (timestamp%1000)*1000000)
 }
 
@@ -103,17 +116,5 @@ func FromTimestampToDateTime(timestamp int64) time.Time {
 //  @return int64
 //
 func ToTimestamp(t time.Time) int64 {
-	return t.UnixNano() / 1000000
-}
-
-//
-// GetTodayMinTimestamp
-//  @Description: 今日零点毫秒
-//  @return int64
-//
-func GetTodayMinTimestamp() int64 {
-	timeStr := time.Now().Format("2006-01-02")
-	fmt.Println("timeStr:", timeStr)
-	t, _ := time.Parse("2006-01-02", timeStr)
 	return t.UnixNano() / 1000000
 }
