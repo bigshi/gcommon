8bfe226b3f76eff4d9619f72e28515609c55bbbf
luc <lcmusic1994@gmail.com>
2023-11-06 17:07:48 +0800

    init

--- pkg/gentity/sql_entity.go
+++ pkg/gentity/sql_entity.go
@@ -2,15 +2,15 @@ package gentity
 
 const SelectSQLTemplate = "select %s from %s.%s %s %s %s" //  1-字段，2-库名，3-表名，4-where，5-order, 6-limit
 
-const InsertSQLTemplate = "insert into %s.%s(gmt_create, gmt_modified, %s) values(now(), now(), %s)" // 1-库名，2-表名，3-字段，4-值
+const InsertSQLTemplate = "insert into %s.%s(%s) values(%s)" // 1-库名，2-表名，3-字段，4-值
 
-const UpdateSQLTemplate = "update %s.%s set %s, gmt_modified = now() where %s" // 1-库名，2-表名，3-字段=值，4-where条件
+const UpdateSQLTemplate = "update %s.%s set %s where %s" // 1-库名，2-表名，3-字段=值，4-where条件
 
 const DeleteSQLTemplate = "delete from %s.%s where %s" // 1-库名，2-表名，3-where条件
 
-const BatchInsertSQLTemplate = "insert into %s.%s(gmt_create, gmt_modified, %s) values %s" // 1-库名，2-表名，3-字段，4-值
+const BatchInsertSQLTemplate = "insert into %s.%s(%s) values %s" // 1-库名，2-表名，3-字段，4-值
 
-const ReplaceSQLTemplate = "replace into %s.%s(gmt_create, gmt_modified, %s) values(now(), now(), %s)" // 1-库名，2-表名，3-字段，4-值
+const ReplaceSQLTemplate = "replace into %s.%s(%s) values(%s)" // 1-库名，2-表名，3-字段，4-值
 
 const Limit string = "limit"
 
