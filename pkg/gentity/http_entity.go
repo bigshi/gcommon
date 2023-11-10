ea43eebfe5761d0202fedb4a515da2143b88a74c
luc <lcmusic1994@gmail.com>
2023-11-10 20:19:52 +0800

    init

--- pkg/gentity/http_entity.go
+++ pkg/gentity/http_entity.go
@@ -7,16 +7,65 @@
 package gentity
 
 import (
+	"encoding/json"
 	"github.com/qionggemens/gcommon/pkg/glog"
+	"io"
 	"net/http"
 	"runtime/debug"
 	"time"
 )
 
+const ErrorCode = 500
+const SuccessCode = 200
+
+type ApiResult struct {
+	Code    int32       `json:"code"`
+	Success bool        `json:"success"`
+	Msg     string      `json:"msg"`
+	Data    interface{} `json:"data"`
+}
+
+func NewOkResult(data interface{}, msg string) ApiResult {
+	result := ApiResult{
+		Msg:     msg,
+		Code:    SuccessCode,
+		Success: true,
+		Data:    data,
+	}
+	return result
+}
+
+func NewOkResultBytes(data interface{}, msg string) []byte {
+	bytes, _ := json.Marshal(NewOkResult(data, msg))
+	return bytes
+}
+
+func NewFailResult(msg string) ApiResult {
+	result := ApiResult{
+		Msg:     msg,
+		Code:    ErrorCode,
+		Success: false,
+	}
+	return result
+}
+
+func NewFailResultBytes(msg string) []byte {
+	bytes, _ := json.Marshal(NewFailResult(msg))
+	return bytes
+}
+
+func WriteOkResponse(rw http.ResponseWriter, data interface{}, msg string) {
+	io.WriteString(rw, string(NewOkResultBytes(data, msg)))
+}
+
+func WriteFailResponse(rw http.ResponseWriter, msg string) {
+	io.WriteString(rw, string(NewFailResultBytes(msg)))
+}
+
 // HttpProxy http代理
 type HttpProxy struct {
-	interceptors []HandleInterceptor
-	requestMap   map[string]func(rw http.ResponseWriter, req *http.Request)
+	Interceptors []HandleInterceptor
+	RequestMap   map[string]func(rw http.ResponseWriter, req *http.Request)
 }
 
 // HandleInterceptor 拦截器
@@ -34,14 +83,14 @@ func (pxy *HttpProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	urlPath := req.URL.Path
 	glog.Infof("ACCESS req begin - path:%s", urlPath)
 	bt := time.Now()
-	reqMapping, isOk := pxy.requestMap[urlPath]
+	reqMapping, isOk := pxy.RequestMap[urlPath]
 	if !isOk {
 		glog.Errorf("ACCESS req end - msg:404, path:%s", req.URL.Path)
 		rw.WriteHeader(http.StatusNotFound)
 		return
 	}
-	for i := 0; i < len(pxy.interceptors); i++ {
-		interceptor := pxy.interceptors[i]
+	for i := 0; i < len(pxy.Interceptors); i++ {
+		interceptor := pxy.Interceptors[i]
 		isOk = interceptor.PreHandle(rw, req)
 		if !isOk {
 			glog.Errorf("ACCESS req end - msg:preHandle fail, path:%s, interceptor:%v", req.URL.Path, interceptor)
@@ -49,8 +98,8 @@ func (pxy *HttpProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		}
 	}
 	reqMapping(rw, req)
-	for i := len(pxy.interceptors) - 1; i >= 0; i-- {
-		interceptor := pxy.interceptors[i]
+	for i := len(pxy.Interceptors) - 1; i >= 0; i-- {
+		interceptor := pxy.Interceptors[i]
 		interceptor.PostHandle(rw, req)
 	}
 	glog.Infof("ACCESS req end - cost:%dms, path:%s", time.Since(bt).Milliseconds(), req.URL.Path)
