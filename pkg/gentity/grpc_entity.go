e46d9bd229b3bc3430734a60eb794855cad259bc
luc <lcmusic1994@gmail.com>
2023-11-13 14:43:53 +0800

    init

--- pkg/gentity/grpc_entity.go
+++ pkg/gentity/grpc_entity.go
@@ -25,21 +25,60 @@ func getBodyStr(body interface{}) string {
 	return bodyStr
 }
 
-func GrpcInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+// GrpcServerInterceptor
+//
+//	@Description: 服务端拦截器
+//	@param ctx
+//	@param req
+//	@param info
+//	@param handler
+//	@return interface{}
+//	@return error
+func GrpcServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	reqStr := getBodyStr(req)
 	defer func() {
 		if p := recover(); p != nil {
 			glog.Errorf("[GRPC ACCESS] %s fail - req:%s, err:%v, stack:%s", info.FullMethod, reqStr, p, string(debug.Stack()))
 		}
 	}()
-	glog.Infof("[GRPC ACCESS] %s param - req:%s", info.FullMethod, reqStr)
+	glog.Infof("[GRPC SERVER] %s param - req:%s", info.FullMethod, reqStr)
 	bt := time.Now()
 	rsp, err := handler(ctx, req)
 	if err != nil {
-		glog.Errorf("[GRPC ACCESS] %s fail - cost:%dms, req:%s, msg:%s", info.FullMethod, time.Since(bt).Milliseconds(), reqStr, err.Error())
+		glog.Errorf("[GRPC SERVER] %s fail - cost:%dms, req:%s, msg:%s", info.FullMethod, time.Since(bt).Milliseconds(), reqStr, err.Error())
 	} else {
 		rspStr := getBodyStr(rsp)
-		glog.Infof("[GRPC ACCESS] %s success - cost:%dms, req:%s, rsp:%s", info.FullMethod, time.Since(bt).Milliseconds(), reqStr, rspStr)
+		glog.Infof("[GRPC SERVER] %s success - cost:%dms, req:%s, rsp:%s", info.FullMethod, time.Since(bt).Milliseconds(), reqStr, rspStr)
 	}
 	return rsp, err
+}
+
+// GrpcClientInterceptor
+//
+//	@Description: 客户端拦截器
+//	@param ctx
+//	@param method
+//	@param req
+//	@param reply
+//	@param cc
+//	@param invoker
+//	@param opts
+//	@return error
+func GrpcClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	reqStr := getBodyStr(req)
+	defer func() {
+		if p := recover(); p != nil {
+			glog.Errorf("[GRPC CLIENT] %s fail - req:%s, err:%v, stack:%s", method, reqStr, p, string(debug.Stack()))
+		}
+	}()
+	glog.Infof("[GRPC CLIENT] %s param - req:%s", method, reqStr)
+	bt := time.Now()
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	if err != nil {
+		glog.Errorf("[GRPC CLIENT] %s fail - cost:%dms, req:%s, msg:%s", method, time.Since(bt).Milliseconds(), reqStr, err.Error())
+	} else {
+		rspStr := getBodyStr(reply)
+		glog.Infof("[GRPC CLIENT] %s success - cost:%dms, req:%s", method, time.Since(bt).Milliseconds(), reqStr, rspStr)
+	}
+	return err
 }
