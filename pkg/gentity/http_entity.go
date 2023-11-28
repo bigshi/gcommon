52492402a38e5afbc19c5f98458bf4b587829735
luc <lcmusic1994@gmail.com>
2023-11-28 10:38:54 +0800

    init

--- pkg/gentity/http_entity.go
+++ pkg/gentity/http_entity.go
@@ -74,7 +74,7 @@ type HandleInterceptor interface {
 	PostHandle(rw http.ResponseWriter, req *http.Request)
 }
 
-func (pxy *HttpServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+func (server *HttpServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if e := recover(); e != nil {
 			glog.Errorf("[HTTP ACCESS] handle fail - path:%s, err:%v, stack:%s", req.URL.Path, e, string(debug.Stack()))
@@ -83,14 +83,14 @@ func (pxy *HttpServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	urlPath := req.URL.Path
 	glog.Infof("[HTTP ACCESS] req begin - path:%s", urlPath)
 	bt := time.Now()
-	reqMapping, isOk := pxy.RequestMap[urlPath]
+	reqMapping, isOk := server.RequestMap[urlPath]
 	if !isOk {
 		glog.Errorf("[HTTP ACCESS] req end - path:%s, msg:404", req.URL.Path)
 		rw.WriteHeader(http.StatusNotFound)
 		return
 	}
-	for i := 0; i < len(pxy.Interceptors); i++ {
-		interceptor := pxy.Interceptors[i]
+	for i := 0; i < len(server.Interceptors); i++ {
+		interceptor := server.Interceptors[i]
 		isOk = interceptor.PreHandle(rw, req)
 		if !isOk {
 			glog.Errorf("[HTTP ACCESS] req end - path:%s, interceptor:%v, msg:preHandle fail", req.URL.Path, interceptor)
@@ -98,8 +98,8 @@ func (pxy *HttpServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		}
 	}
 	reqMapping(rw, req)
-	for i := len(pxy.Interceptors) - 1; i >= 0; i-- {
-		interceptor := pxy.Interceptors[i]
+	for i := len(server.Interceptors) - 1; i >= 0; i-- {
+		interceptor := server.Interceptors[i]
 		interceptor.PostHandle(rw, req)
 	}
 	glog.Infof("[HTTP ACCESS] req end - cost:%dms, path:%s", time.Since(bt).Milliseconds(), req.URL.Path)
