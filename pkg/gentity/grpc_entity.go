2662da5bac13012d595c86ccd9917e5f8e404ae9
luc <lcmusic1994@gmail.com>
2023-12-05 17:28:26 +0800

    init

--- pkg/gentity/grpc_entity.go
+++ pkg/gentity/grpc_entity.go
@@ -12,6 +12,8 @@ import (
 	"github.com/qionggemens/gcommon/pkg/glog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+	"net"
 	"runtime/debug"
 	"strconv"
 	"time"
@@ -67,6 +69,17 @@ func getMdOfServer(ctx context.Context) metadata.MD {
 	return md
 }
 
+func getAddr(ctx context.Context) string {
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return ""
+	}
+	if p.Addr == net.Addr(nil) {
+		return ""
+	}
+	return p.Addr.String()
+}
+
 // GrpcServerInterceptor
 //
 //	@Description: 服务端拦截器
@@ -78,20 +91,21 @@ func getMdOfServer(ctx context.Context) metadata.MD {
 //	@return error
 func GrpcServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	reqStr := getBodyStr(req)
+	clientAddr := getAddr(ctx)
 	md := getMdOfServer(ctx)
 	defer func() {
 		if p := recover(); p != nil {
-			glog.Errorf("[GRPC SERVER] %s fail - md:%+v, req:%s, err:%v, stack:%s", info.FullMethod, md, reqStr, p, string(debug.Stack()))
+			glog.Errorf("[GRPC SERVER] %s fail [From:%s] - md:%+v, req:%s, err:%v, stack:%s", info.FullMethod, clientAddr, md, reqStr, p, string(debug.Stack()))
 		}
 	}()
-	glog.Infof("[GRPC SERVER] %s param - md:%+v, req:%s", info.FullMethod, md, reqStr)
+	glog.Infof("[GRPC SERVER] %s begin [From:%s] - md:%+v, req:%s", info.FullMethod, clientAddr, md, reqStr)
 	bt := time.Now()
 	rsp, err := handler(ctx, req)
 	if err != nil {
-		glog.Errorf("[GRPC SERVER] %s fail - cost:%dms, md:%+v, req:%s, msg:%s", info.FullMethod, md, time.Since(bt).Milliseconds(), reqStr, err.Error())
+		glog.Errorf("[GRPC SERVER] %s fail [From:%s] - cost:%dms, md:%+v, req:%s, msg:%s", info.FullMethod, clientAddr, md, time.Since(bt).Milliseconds(), reqStr, err.Error())
 	} else {
 		rspStr := getBodyStr(rsp)
-		glog.Infof("[GRPC SERVER] %s success - cost:%dms, md:%+v, req:%s, rsp:%s", info.FullMethod, md, time.Since(bt).Milliseconds(), reqStr, rspStr)
+		glog.Infof("[GRPC SERVER] %s success [From:%s] - cost:%dms, md:%+v, req:%s, rsp:%s", info.FullMethod, clientAddr, md, time.Since(bt).Milliseconds(), reqStr, rspStr)
 	}
 	return rsp, err
 }
@@ -109,20 +123,21 @@ func GrpcServerInterceptor(ctx context.Context, req interface{}, info *grpc.Unar
 //	@return error
 func GrpcClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	reqStr := getBodyStr(req)
+	serverAddr := getAddr(ctx)
 	outCtx, md := getMdOfClient(ctx)
 	defer func() {
 		if p := recover(); p != nil {
-			glog.Errorf("[GRPC CLIENT] %s fail - md:%+v, req:%s, err:%v, stack:%s", method, md, reqStr, p, string(debug.Stack()))
+			glog.Errorf("[GRPC CLIENT] %s fail [To:%s] - md:%+v, req:%s, err:%v, stack:%s", method, serverAddr, md, reqStr, p, string(debug.Stack()))
 		}
 	}()
-	glog.Infof("[GRPC CLIENT] %s param - md:%+v, req:%s", method, md, reqStr)
+	glog.Infof("[GRPC CLIENT] %s begin [To:%s] - md:%+v, req:%s", method, serverAddr, md, reqStr)
 	bt := time.Now()
 	err := invoker(outCtx, method, req, reply, cc, opts...)
 	if err != nil {
-		glog.Errorf("[GRPC CLIENT] %s fail - cost:%dms, md:%+v, req:%s, msg:%s", method, md, time.Since(bt).Milliseconds(), reqStr, err.Error())
+		glog.Errorf("[GRPC CLIENT] %s fail [To:%s] - cost:%dms, md:%+v, req:%s, msg:%s", method, serverAddr, md, time.Since(bt).Milliseconds(), reqStr, err.Error())
 	} else {
 		rspStr := getBodyStr(reply)
-		glog.Infof("[GRPC CLIENT] %s success - cost:%dms, md:%+v, req:%s, rsp:%s", method, md, time.Since(bt).Milliseconds(), reqStr, rspStr)
+		glog.Infof("[GRPC CLIENT] %s success [To:%s] - cost:%dms, md:%+v, req:%s, rsp:%s", method, serverAddr, md, time.Since(bt).Milliseconds(), reqStr, rspStr)
 	}
 	return err
 }
