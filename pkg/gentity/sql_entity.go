25c84a2a1cbaf1c7eeb2d29e60b0ce0bef2a7e03
luc <lcmusic1994@gmail.com>
2023-12-22 11:00:39 +0800

    init

--- pkg/gentity/sql_entity.go
+++ pkg/gentity/sql_entity.go
@@ -4,3 +4,14 @@ type QueryCondition struct {
 	QueryKey   string
 	QueryValue interface{}
 }
+
+const (
+	OrderMapKeyOfCreateTime = "create_time"
+	OrderMapKeyOfUpdateTime = "update_time"
+	OrderMapKeyOfId         = "id"
+
+	OrderMapValueOfAsc  = "asc"
+	OrderMapValueOfDesc = "desc"
+
+	QueryLimit = 300
+)
